256d5f7271a5b1bb6e8415117ab576e19cef5075
tom <80106862+spiiderss@users.noreply.github.com>
2021-07-17 08:41:15 +0800

    Add files via upload

---
+++ constants/googleKyes.js
@@ -0,0 +1,1 @@
+export default GoogleKyes = 'AIzaSyCKatM4hdL8g5dT7mbB4JMXqNc4xtEcGkc';
---
+++ constants/index.js
@@ -0,0 +1,6 @@
+import GOOGLE_API_KEY from './googleKyes';
+import icons from './icons';
+import images from './images';
+import {COLORS, SIZES, FONTS} from './theme';
+
+export {GOOGLE_API_KEY, COLORS, SIZES, FONTS, images, icons};
